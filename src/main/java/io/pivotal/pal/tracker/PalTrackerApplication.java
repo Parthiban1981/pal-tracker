a2a257be67bf29fbf2e9fefde40d8b65ec6ee397
sashinpivotal <sashin@pivotal.io>
2018-11-20 20:36:24 -0500

    Spring Security

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -8,9 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
+import org.springframework.context.annotation.ComponentScan;
 import javax.sql.DataSource;
 
+@ComponentScan(basePackages = "io.pivotal.*")
 @SpringBootApplication
 public class PalTrackerApplication {
 
